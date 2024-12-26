6ea39faf7796e5ec819689ea76c9108acca3e183
DasunNirmal <dasun200301@gmail.com>
2024-12-27 00:44:34 +0530

    Updated And Renamed

--- src/store/ItemProvider.tsx
+++ src/store/ItemProvider.tsx
@@ -1,13 +1,13 @@
-import {createContext, useState} from "react";
-import {Item} from "../models/Item.ts";
+import {createContext, useReducer, useState} from "react";
+import {initialState, ItemReducer} from "../reducers/ItemReducer.ts";
 
 export const ItemContext = createContext();
 
 export function ItemProvider({children}) {
-    const [item, setItem] = useState<Item[]>([]);
+    const [item, dispatch] = useReducer(ItemReducer, initialState);
 
     return (
-        <ItemContext.Provider value={[item, setItem]}>
+        <ItemContext.Provider value={[item, dispatch]}>
             {children}
         </ItemContext.Provider>
     );
