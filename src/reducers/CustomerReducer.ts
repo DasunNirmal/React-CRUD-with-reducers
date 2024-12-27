5e60f28d80ccc02f06db1f3c7eb454b36450a5ef
DasunNirmal <dasun200301@gmail.com>
2024-12-28 00:33:08 +0530

    customer update done

--- src/reducers/CustomerReducer.ts
+++ src/reducers/CustomerReducer.ts
@@ -5,5 +5,8 @@ export function CustomerReducer(state:Customer[], action:{type:string, payload:C
     switch(action.type){
         case 'ADD_CUSTOMER':
             return [...state, action.payload];
+            case 'UPDATE_CUSTOMER':
+                return state.map((customers:Customer) => customers.email === action.payload.email ?
+                    {...customers, name:action.payload.name,email:action.payload.email,phone:action.payload.phone} : customers);
     }
 }
