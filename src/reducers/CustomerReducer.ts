8f793b3a4638877c51a212ff1afce34efbe22a2d
DasunNirmal <dasun200301@gmail.com>
2024-12-28 14:59:35 +0530

    customer delete updated

--- src/reducers/CustomerReducer.ts
+++ src/reducers/CustomerReducer.ts
@@ -5,8 +5,12 @@ export function CustomerReducer(state:Customer[], action:{type:string, payload:C
     switch(action.type){
         case 'ADD_CUSTOMER':
             return [...state, action.payload];
-            case 'UPDATE_CUSTOMER':
-                return state.map((customers:Customer) => customers.email === action.payload.email ?
-                    {...customers, name:action.payload.name,email:action.payload.email,phone:action.payload.phone} : customers);
+        case 'UPDATE_CUSTOMER':
+            return state.map((customers:Customer) => customers.email === action.payload.email ?
+                {...customers, name:action.payload.name,email:action.payload.email,phone:action.payload.phone} : customers);
+        case 'DELETE_CUSTOMER':
+            return (customers:Customer[]) => customers.slice(0,-1);
+        default:
+            return state;
     }
 }
