b42b64dbab1614640cfca93751c943bdf90cf43a
DasunNirmal <dasun200301@gmail.com>
2024-12-29 20:31:37 +0530

    id attribute added to each inputs

--- src/component/ItemModal.tsx
+++ src/component/ItemModal.tsx
@@ -1,11 +1,11 @@
 export function ItemModal(props, children) {
     return (
         <>
-            <input className="mb-6 p-2" type="text" placeholder="Item COde"
+            <input id='code' className="mb-6 p-2" type="text" placeholder="Item Code"
                    onChange={(e) => props.setCode(e.target.value)}/>
-            <input className="mb-6 p-2" type="text" placeholder="Item Name"
+            <input id='itemName' className="mb-6 p-2" type="text" placeholder="Item Name"
                    onChange={(e) => props.setItemName(e.target.value)}/>
-            <input className="mb-6 p-2" type="text" placeholder="Item Quantity"
+            <input id='qty' className="mb-6 p-2" type="text" placeholder="Item Quantity"
                    onChange={(e) => props.setQty(e.target.value)}/>
             <button onClick={props.handleSubmit}>{props.children}</button>
         </>
