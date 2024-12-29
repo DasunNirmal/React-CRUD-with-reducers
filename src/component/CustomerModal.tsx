9492a41839a3bf72da08bd840adf61859b0d0fb8
DasunNirmal <dasun200301@gmail.com>
2024-12-29 20:12:33 +0530

    id attribute added to each input

--- src/component/CustomerModal.tsx
+++ src/component/CustomerModal.tsx
@@ -1,11 +1,11 @@
 export function CustomerModal(props, children) {
     return (
         <>
-            <input className="mb-6 p-2" type="text" placeholder="Name"
+            <input id='name' className="mb-6 p-2" type="text" placeholder="Name"
                    onChange={(e) => props.setName(e.target.value)}/>
-            <input className="mb-6 p-2" type="text" placeholder="Email"
+            <input id='emial' className="mb-6 p-2" type="text" placeholder="Email"
                    onChange={(e) => props.setEmail(e.target.value)}/>
-            <input className="mb-6 p-2" type="text" placeholder="Phone Number"
+            <input id='phone' className="mb-6 p-2" type="text" placeholder="Phone Number"
                    onChange={(e) => props.setPhone(e.target.value)}/>
             <button onClick={props.handleSubmit}>{props.children}</button>
         </>
