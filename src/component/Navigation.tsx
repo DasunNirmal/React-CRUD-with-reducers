10da317dc5915ff9a3e5c1875375831d11017e7a
DasunNirmal <dasun200301@gmail.com>
2025-01-01 18:14:32 +0530

    Done

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -25,9 +25,9 @@ export function Navigation() {
                 <nav className="flex space-x-4 justify-center">
                     <ul className="flex space-x-4 text-white">
                         <li><Link className={activePage('dashboard')} to={''}>Dashboard</Link></li>
-                        <li><Link className={activePage('add')} to={'/add'}>Add Customer</Link></li>
-                        <li><Link className={activePage('update')} to={'/update'}>Update Customer</Link></li>
-                        <li><Link className={activePage('delete')} to={'/delete'}>Delete Customer</Link></li>
+                        <li><Link className={activePage('add')} to={'/add'}>Save</Link></li>
+                        <li><Link className={activePage('update')} to={'/update'}>Update</Link></li>
+                        <li><Link className={activePage('delete')} to={'/delete'}>Delete</Link></li>
                     </ul>
                 </nav>
             </header>
